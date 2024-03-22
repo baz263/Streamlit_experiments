8424d955c0ec737fe61c8c3673c2b15f3fc50c4f
harbarbar <baz263@gmail.com>
2024-03-22 22:24:49 +0100

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,25 +5,6 @@ import pandas as pd
 from google.oauth2 import service_account
 
 
-# def get_data(sheet_name):
-#     # Authenticate and open the Google Sheet
-#     credentials_dict = st.secrets['gcp_service_account']
-#     credentials = service_account.Credentials.from_service_account_info(credentials_dict, scopes=[
-#         "https://www.googleapis.com/auth/spreadsheets",
-#         "https://www.googleapis.com/auth/drive",
-#     ])
-#     gc= gspread.client(auth=credentials)
-#     wks = gc.open(sheet_name).sheet1
-
-#     data= wks.get_all_values()
-#     column_names = data[0]
-#     df = pd.DataFrame(data[1:], columns=column_names)
-#     return df
-
-# data= get_data('nearest_asteroid')
-# st.write('boob')
-# st.dataframe(data)
-
 def get_data(sheet_name):
     # Authenticate and open the Google Sheet
     credentials_dict = st.secrets['gcp_service_account']
@@ -31,15 +12,34 @@ def get_data(sheet_name):
         "https://www.googleapis.com/auth/spreadsheets",
         "https://www.googleapis.com/auth/drive",
     ])
-    gc = gspread.Client(auth=credentials)  # Change gspread.client to gspread.Client
-    gc.login()  # Optional: You can call login() to authenticate if needed
+    gc= gspread.Client(auth=credentials)
     wks = gc.open(sheet_name).sheet1
 
-    data = wks.get_all_values()
+    data= wks.get_all_values()
     column_names = data[0]
     df = pd.DataFrame(data[1:], columns=column_names)
     return df
 
-data = get_data('nearest_asteroid')
+data= get_data('nearest_asteroid')
 st.write('boob')
 st.dataframe(data)
+
+# def get_data(sheet_name):
+#     # Authenticate and open the Google Sheet
+#     credentials_dict = st.secrets['gcp_service_account']
+#     credentials = service_account.Credentials.from_service_account_info(credentials_dict, scopes=[
+#         "https://www.googleapis.com/auth/spreadsheets",
+#         "https://www.googleapis.com/auth/drive",
+#     ])
+#     gc = gspread.Client(auth=credentials)  # Change gspread.client to gspread.Client
+#     gc.login()  # Optional: You can call login() to authenticate if needed
+#     wks = gc.open(sheet_name).sheet1
+
+#     data = wks.get_all_values()
+#     column_names = data[0]
+#     df = pd.DataFrame(data[1:], columns=column_names)
+#     return df
+
+# data = get_data('nearest_asteroid')
+# st.write('boob')
+# st.dataframe(data)
