76284e8fb6bbecfa28310ac15adee61bfb44a1a2
harbarbar <baz263@gmail.com>
2024-03-22 15:59:46 +0100

    st.write added

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 import pandas as pd
 
-
+st.write('cunt')
 scopes = ['https://docs.google.com/spreadsheets/d/19eKVNRzY_h56ASeLxu-aR_dTOkHbzrA6Msg57F1_ebs/edit?usp=sharing']
 skey = st.secrets['gcp_service_account']
 credentials = Credentials.from_service_account_info(skey, scopes=scopes)
