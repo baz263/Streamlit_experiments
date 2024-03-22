b84934ff229c4bd3a3e5974d6792130810b66aa1
harbarbar <baz263@gmail.com>
2024-03-22 16:18:25 +0100

    cunt

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,21 +4,23 @@ from google.oauth2.service_account import Credentials
 import pandas as pd
 
 
-def get_data(sheet_name):
-    # Authenticate and open the Google Sheet
-    credentials_dict = st.secrets['gcp_service_account']
-    credentials = service_account.Credentials.from_service_account_info(credentials_dict, scopes=[
-        "https://www.googleapis.com/auth/spreadsheets",
-        "https://www.googleapis.com/auth/drive",
-    ])
-    gc= gspread.client(auth=credentials)
-    wks = gc.open(sheet_name).sheet1
+# def get_data(sheet_name):
+#     # Authenticate and open the Google Sheet
+#     credentials_dict = st.secrets['gcp_service_account']
+#     credentials = service_account.Credentials.from_service_account_info(credentials_dict, scopes=[
+#         "https://www.googleapis.com/auth/spreadsheets",
+#         "https://www.googleapis.com/auth/drive",
+#     ])
+#     gc= gspread.client(auth=credentials)
+#     wks = gc.open(sheet_name).sheet1
 
-    data= wks.get_all_values()
-    column_names = data[0]
-    df = pd.DataFrame(data[1:], columns=column_names)
-    return df
+#     data= wks.get_all_values()
+#     column_names = data[0]
+#     df = pd.DataFrame(data[1:], columns=column_names)
+#     return df
 
-data= get_data('nearest_asteroid')
-st.write('boob')
-st.dataframe(data)
+# data= get_data('nearest_asteroid')
+# st.write('boob')
+# st.dataframe(data)
+
+st.write('cunt')
